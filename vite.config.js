dcce43c1aebab4c06795410ea168ff37d7cfd67b
Aaryf khan <aaryf@Aaryfs-MacBook-Air.local>
2025-01-05 14:30:26 +0530

    frontend assignment

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,7 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
-// Vite config with Vitest setup for handling React files
+// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  test: {
-    environment: 'jsdom', // Make sure jsdom is set for React tests
-    globals: true,         // Use global variables like `describe`, `it`, etc.
-    transformMode: {
-      web: [/.[tj]sx$/],  // Allow .jsx and .tsx files to be transformed
-    },
-    coverage: {
-      reporter: ['text', 'json', 'html'], // Optional: Collect code coverage
-    },
-  },
-});
+})
