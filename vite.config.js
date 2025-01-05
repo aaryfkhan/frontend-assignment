076be5d8fc91ded1d50a288eba2c9a07705910e1
Aaryf khan <aaryf@Aaryfs-MacBook-Air.local>
2025-01-05 14:46:35 +0530

    frontend assignment

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite';
-
+import react from '@vitejs/plugin-react'
 export default defineConfig({
+  plugins: [react()],
   build: {
     outDir: 'dist', // This should be the output folder you deploy
     rollupOptions: {
