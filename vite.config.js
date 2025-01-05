3e8bf137059bf0a6d60068d800dfb40a972a3069
Aaryf khan <aaryf@Aaryfs-MacBook-Air.local>
2025-01-05 15:16:37 +0530

    frontend assignment

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,5 @@
-import { defineConfig } from 'vite';
+import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
-  build: {
-    outDir: 'dist', // This should be the output folder you deploy
-    rollupOptions: {
-      input: 'index.html', // Ensure this points to your HTML entry file
-    },
-  },
-  server: {
-    // Ensure correct handling for dev mode
-    mimeTypes: {
-      '.js': 'application/javascript', // Explicitly tell Vite to serve .js files correctly
-    },
-  },
-});
+})
